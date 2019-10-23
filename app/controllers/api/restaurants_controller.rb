921aaa83ff55a67dcc82f00c0e4a0b556889be87
Merak1 <eddie_comes_for_you@hotmail.es>
2019-10-22 19:48:43 -0500

    CRUD operations (create,read,update,delete) now availabe

--- app/controllers/api/restaurants_controller.rb
+++ app/controllers/api/restaurants_controller.rb
@@ -1,8 +1,8 @@
 module Api
     # module V1
-        class  RestaurantsController < ApplicationController 
+        class  RestaurantsController < ApplicationController
             def index
-                    restaurants = Restaurant.order('created_at DESC');
+                restaurants = Restaurant.order('created_at DESC');
                 render json: {status: 'SUCCESS', message: 'Loaded Restaurants', data:restaurants},status: :ok
                 # render plain: "OK"
             end
@@ -10,6 +10,47 @@ module Api
                 restaurant = Restaurant.find(params[:id])
                 render json: {status: 'SUCCESS', message: 'Loaded Restaurant', data:restaurant},status: :ok
             end
+            def create 
+                restaurant = Restaurant.new(restaurant_params)
+
+                if restaurant.save
+                  render json: {status: 'SUCCESS', message: 'Saved Restaurant', data:restaurant},status: :ok
+                else
+                  render json: {status: 'ERROR', message: 'Restaurant not saved', data:restaurant.errors},status: :unprocessable_entity
+                end
+            end
+            def destroy
+              restaurant = Restaurant.find(params[:id])
+              restaurant.destroy
+              render json: {status: 'SUCCESS', message: 'Deleted Restaurant', data:restaurant},status: :ok
+            end
+            def update 
+              restaurant = Restaurant.find(params[:id])
+              if restaurant.update_attributes(restaurant_params)
+                render json: {status: 'SUCCESS', message: 'Updated Restaurant', data:restaurant},status: :ok
+              else
+                render json: {status: 'ERROR', message: 'Restaurant not updated', data:restaurant.errors},status: :unprocessable_entity
+              end
+
+            end
+
+            private 
+            def restaurant_params
+              params.permit(
+                :uuid,
+                :rating,
+                :name,
+                :site,
+                :email,
+                :phone,
+                :street,
+                :city,
+                :state,
+                :lat,
+                :lng
+              )
+
         end
-    # end
+    # end       
+end
 end
